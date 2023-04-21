93b8eb40bac6ca62b6ebaacc07a2703426541996
Gileade Kelvin <gileadekelvin@gmail.com>
2023-04-21 20:48:32 -0300

    fix: add basic support for query params

--- app/camara/[...slug]/page.tsx
+++ app/camara/[...slug]/page.tsx
@@ -10,8 +10,24 @@ async function getData(url: string) {
   return data;
 }
 
-export default async function Page({ params }: { params: { slug: string[] } }) {
-  const url = `https://dadosabertos.camara.leg.br/api/v2/${params.slug?.join('/')}`;
+function objectToQueryParams(obj: { [key: string]: any } | undefined): string {
+  if (!obj) {
+    return '';
+  }
+  const keys = Object.keys(obj);
+  const params = keys.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`);
+  return params.join('&');
+}
+
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string[] };
+  searchParams?: { [key: string]: any };
+}) {
+  const queryParams = objectToQueryParams(searchParams);
+  const url = `https://dadosabertos.camara.leg.br/api/v2/${params.slug?.join('/')}?${queryParams}`;
   const data = await getData(url);
 
   return <JsonViewerComponent data={data} url={url} />;
