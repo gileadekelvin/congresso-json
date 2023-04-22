10bf3a602bdafce3777128ccbbcf380f4455716a
Gileade Kelvin <gileadekelvin@gmail.com>
2023-04-21 21:06:19 -0300

    fix: allow to use query params in senado

--- app/senado/[...slug]/page.tsx
+++ app/senado/[...slug]/page.tsx
@@ -10,8 +10,36 @@ async function getData(url: string) {
   return data;
 }
 
-export default async function Page({ params }: { params: { slug: string[] } }) {
-  const url = `https://legis.senado.leg.br/dadosabertos/${params.slug?.join('/')}.json`;
+function objectToQueryParams(obj: { [key: string]: any } | undefined): string {
+  if (!obj) {
+    return '';
+  }
+  const keys = Object.keys(obj);
+  const params = keys
+    .map((key) => {
+      if (key !== 'slug') {
+        return `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`;
+      }
+      return null;
+    })
+    .filter((k) => !!k);
+
+  if (params.length > 0) {
+    return `&${params.join('&')}`;
+  }
+  return '';
+}
+
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string[] };
+  searchParams?: { [key: string]: any };
+}) {
+  const queryParams = objectToQueryParams(searchParams);
+  const url = `https://legis.senado.leg.br/dadosabertos/${params.slug?.join('/')}.json${queryParams}`;
+  console.log(url);
   const data = await getData(url);
 
   return <JsonViewerComponent data={data} url={url} />;
